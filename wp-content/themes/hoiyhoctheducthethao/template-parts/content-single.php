3096f8814217f5e1a5256b74af3c506243ddef5d
vankai <75766341+vankai-48f1@users.noreply.github.com>
2022-08-17 18:40:30 +0700

    post single hidden author, text align right date

--- wp-content/themes/hoiyhoctheducthethao/template-parts/content-single.php
+++ wp-content/themes/hoiyhoctheducthethao/template-parts/content-single.php
@@ -4,21 +4,24 @@
 
     <div class="post-lead">
         <!-- Author -->
-        <div class="post-author">
-            <?php $avatar_url = get_avatar_url(get_the_author_meta('ID'), array('size' => 450)); ?>
-            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-                <div class="author-picture" style="background-image: url('<?php echo esc_url($avatar_url); ?>')"></div>
+        <!-- <div class="post-author">
+            < ?php $avatar_url = get_avatar_url(get_the_author_meta('ID'), array('size' => 450)); ?>
+            <a href="< ?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                <div class="author-picture" style="background-image: url('< ?php echo esc_url($avatar_url); ?>')"></div>
             </a>
-            <!-- Date/Time -->
+            Date/Time
             <div class="post-author-info">
                 <div class="post-author-name">
-                    <?php the_author_posts_link() ?>
+                    < ?php the_author_posts_link() ?>
                 </div>
-                <div class="post-date"><?php echo get_the_date('d/m/Y') ?></div>
+                <div class="post-date">< ?php echo get_the_date('d/m/Y') ?></div>
             </div>
-        </div>
+        </div> -->
         <!-- Share -->
-        <div class="post-share"><i class="far fa-share-alt"></i>&ensp;Share</div>
+        <div class="post-share">
+            <div class="post-date"><strong style="color: #000;">Ngày đăng:</strong> <?php echo get_the_date('d/m/Y') ?>&ensp;</div>
+            <div class="post-share-main"><i class="far fa-share-alt"></i>&ensp;Share</div>
+        </div>
     </div>
     <!-- Post Content -->
     <div class="post-body post-body-single">
