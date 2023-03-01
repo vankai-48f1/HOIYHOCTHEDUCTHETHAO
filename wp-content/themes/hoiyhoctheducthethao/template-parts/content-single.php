fa44a1730e46ad9b8f0e3164fe95c2de400e2dc7
vankai <75766341+vankai-48f1@users.noreply.github.com>
2023-03-01 17:42:46 +0700

    add co-author on page single

--- wp-content/themes/hoiyhoctheducthethao/template-parts/content-single.php
+++ wp-content/themes/hoiyhoctheducthethao/template-parts/content-single.php
@@ -3,20 +3,55 @@
     <h1 class="font-primary-bold-40 mb-4"><?php the_title() ?></h1>
 
     <div class="post-lead">
-        <!-- Author -->
-        <!-- <div class="post-author">
-            < ?php $avatar_url = get_avatar_url(get_the_author_meta('ID'), array('size' => 450)); ?>
-            <a href="< ?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-                <div class="author-picture" style="background-image: url('< ?php echo esc_url($avatar_url); ?>')"></div>
-            </a>
-            Date/Time
-            <div class="post-author-info">
-                <div class="post-author-name">
-                    < ?php the_author_posts_link() ?>
+        <div class="post-authors">
+            <!-- Author -->
+            <div class="post-author">
+                <?php
+                // $avatar_url = get_avatar_url(get_the_author_meta('ID'), array('size' => 450));
+                $author_first_avt = get_field('user_avatar', 'user_' . get_the_author_meta('ID'));
+                ?>
+                <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                    <div class="author-picture" style="background-image: url('<?php echo esc_url($author_first_avt); ?>')"></div>
+                </a>
+
+                <div class="post-author-info">
+                    <div class="post-author-name">
+                        <?php the_author_posts_link() ?>
+                    </div>
+                    <div class="post-author-name">
+                        <?php the_author_email() ?>
+                    </div>
                 </div>
-                <div class="post-date">< ?php echo get_the_date('d/m/Y') ?></div>
             </div>
-        </div> -->
+
+            <!-- Co-Authors -->
+            <?php
+            $co_authors = get_field("co-authors");
+            $incre = 0;
+            if ($co_authors) : ?>
+                <?php foreach ($co_authors as $author) : $incre++; ?>
+                    <?php if ($author->ID != get_the_author_meta('ID') && $incre < 2) : ?>
+                        <div class="post-author">
+                            <?php
+                            $author_avt = get_field('user_avatar', 'user_' . $author->ID);
+                            ?>
+                            <a href="<?php echo esc_attr($author->user_url);; ?>">
+                                <div class="author-picture" style="background-image: url('<?php echo esc_url($author_avt); ?>')"></div>
+                            </a>
+
+                            <div class="post-author-info">
+                                <div class="post-author-name">
+                                    <?php echo $author->display_name; ?>
+                                </div>
+                                <div class="post-author-name">
+                                    <?php echo $author->user_email; ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
         <!-- Share -->
         <div class="post-share">
             <div class="post-date"><strong style="color: #000;">Ngày đăng:</strong> <?php echo get_the_date('d/m/Y') ?>&ensp;</div>
